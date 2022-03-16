fcc9bf6aea470930a989cc1a1ae82b11bcf478e8
foxrw1 <78601276+foxrw1@users.noreply.github.com>
2022-03-16 10:02:19 -0400

    Add files via upload

---
+++ Vote By Text.cpp
@@ -0,0 +1,42 @@
+// Vote By Text.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+    //Create a vector to hold the votes
+  
+
+    ifstream fin;
+    fin.open("votes.txt");
+
+
+
+    while (!fin.eof()) {
+        //Input phone number of the voter
+        string phone;
+        fin >> phone;
+        //Input the voter's vote
+        string vote;
+        fin >> vote;
+        
+        // Part C. Check to see if the phone number has already voted
+
+  
+        // Part B. Add the phone number to the vector
+          
+
+        // Part A. Count the votes
+
+     
+    }//repeat until the end of the file
+
+    cout << "Should we launch a mission to see if the moon is made of cheese?" << endl << endl;
+
+    system("pause");
+}
